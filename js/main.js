61c88b4eed420fbc16884ae2506e0327fc27afc3
Duckerty <Pahan14420@gmail.com>
2022-06-06 12:24:59 +0300

    fixed mobile menu bug

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('.header__menu-click').click(function(){
     $('.nav-links-row-mobile').addClass('nav-links-row-mobile--active');
+    $('.nav-links').addClass('nav-links--active');
   });
   $('.header__menu-close--btn').click(function(){
     $('.nav-links ul').removeClass('nav-links-row-mobile--active');
