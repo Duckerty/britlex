30caeee41321b188814898271d61fee87816a1af
Duckerty <Pahan14420@gmail.com>
2022-06-06 12:18:18 +0300

    maded header and language sections adaptive, added mobile burger menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.header__menu-click').click(function(){
+    $('.nav-links-row-mobile').addClass('nav-links-row-mobile--active');
+  });
+  $('.header__menu-close--btn').click(function(){
+    $('.nav-links ul').removeClass('nav-links-row-mobile--active');
+  });
+  $('.header__menu-list__link').click(function(){
+    $('.nav-links ul').removeClass('nav-links-row-mobile--active');
+  });
+});
